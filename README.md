84a2b1a9f7bbc89101b6be5a6bb1b140b2c6404e
Matthias <m@tth.es>
2019-10-30 08:25:59 +0100

    update

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# kickstart-flavor sdi 
+
+see (http://github.com/infracamp/kickstart) for more information.
+
+Document Index:
+
+- [Development Guide for this flavor](DEVELOPMENT.md)
+- [Dockerhub page](https://hub.docker.com/r/infracamp/kickstart-flavor-sdi/)
+    - [Tags available](https://hub.docker.com/r/infracamp/kickstart-flavor-sdi/tags/)
+    - [Build details](https://hub.docker.com/r/infracamp/kickstart-flavor-sdi/builds/)
+
+
+## Contains
+
+- `gcloud`
+- `docker-machine`
+- `terraform`
+
---
+++ flavor/flavor-build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#
+# This file is called from Kickstart to
+# finalize the container installation
+#
+
+set -e
+
+sudo apt-get update && sudo apt-get -y install lsb-release gnupg2 docker.io pwgen ccrypt
+
+# Create environment variable for correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# Update the package list and install the Cloud SDK
+sudo apt-get update && sudo apt-get -y install google-cloud-sdk
+
+base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
+  chmod +x /usr/local/bin/docker-machine
+
