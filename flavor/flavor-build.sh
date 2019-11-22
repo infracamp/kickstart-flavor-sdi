28aa8ba14dacac814435acfd940673baf9a1c200
Matthias <m@tth.es>
2019-11-22 10:23:09 +0100

    added azure

--- flavor/flavor-build.sh
+++ flavor/flavor-build.sh
@@ -15,14 +15,31 @@ export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 # Add the Cloud SDK distribution URI as a package source
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
+
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
 # Import the Google Cloud Platform public key
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 # Update the package list and install the Cloud SDK
-sudo apt-get update && sudo apt-get -y install google-cloud-sdk
+sudo apt-get update && sudo apt-get -y install google-cloud-sdk azure-cli
 
 base=https://github.com/docker/machine/releases/download/v0.16.0 &&
   curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
   sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
   chmod +x /usr/local/bin/docker-machine
+
+
+
+
+
+## Install Azure
+
+
 
