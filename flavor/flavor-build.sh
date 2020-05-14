be47aba9b8e40b61f9ee216dd63c058300e95173
Joe Koshy Alex <joe@talpa-solutions.com>
2020-05-14 17:11:06 +0200

    Added mbstring

--- flavor/flavor-build.sh
+++ flavor/flavor-build.sh
@@ -11,7 +11,7 @@ set -e
 
 
 
-sudo apt-get update && sudo apt-get -y install lsb-release gnupg2 docker.io pwgen ccrypt jq awscli iputils-ping dnsutils
+sudo apt-get update && sudo apt-get -y install lsb-release gnupg2 docker.io pwgen ccrypt jq awscli iputils-ping dnsutils php7.2-mbstring
 
 
 
